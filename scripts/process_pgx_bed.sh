e4e61b954a99ee3e10e0bec0fa9952ae9d11053d
Robert Warmerdam <robert_warmerdam@hotmail.com>
2021-12-02 14:13:20 +0100

    Updated main in imputation pipeline

---
+++ scripts/process_pgx_bed.sh
@@ -0,0 +1,14 @@
+ml DBD-mysql/4.048-foss-2018b-Perl-5.28.0
+ml BEDTools/2.28.0-GCCcore-7.3.0
+
+mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg19.chromInfo" | \
+sed '2,$s/chr//g' > hg19.genome
+
+bedtools flank -b 1000000 -i ~/pgx-pipeline/validation/pgx-genes_GRCh37.bed -g hg19.genome > \
+~/pgx-pipeline/validation/pgx-genes_GRCh37_1Mb-flanks.bed
+
+mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e "select chrom, size from hg38.chromInfo" | \
+sed '2,$s/chr//g' > hg38.genome
+
+bedtools flank -b 1000000 -i ~/pgx-pipeline/validation/pgx-genes_GRCh38.bed -g hg38.genome > \
+~/pgx-pipeline/validation/pgx-genes_GRCh38_1Mb-flanks.bed
