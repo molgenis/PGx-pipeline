100570bbbec3f47ed5f398069de3ac8981dbbb57
Robert Warmerdam <robert_warmerdam@hotmail.com>
2021-12-07 11:09:02 +0100

    Renamed important variables

--- protocols/concatenate_chromosomes.sh
+++ protocols/concatenate_chromosomes.sh
@@ -3,7 +3,7 @@
 ### variables to help adding to database (have to use weave)
 ###
 
-#array genotypesPlinkPrefix
+#array genotypesPlinkPrefixArray
 #string concatenatedGenotypesPlinkPrefix
 #string plinkVersion
 
@@ -16,7 +16,7 @@ module load ${plinkVersion}
 #Check modules
 module list
 
-printf "%s\n" "${genotypesPlinkPrefix[@]}" > files_to_merge.txt
+printf "%s\n" "${genotypesPlinkPrefixArray[@]}" > files_to_merge.txt
 
 plink --merge-list files_to_merge.txt --make-bed --out ${concatenatedGenotypesPlinkPrefix}
 
