969c7845fd0944559dd7a8ff65dd14581115ccba
CAWarmerdam <robert_warmerdam@hotmail.com>
2022-03-07 12:24:53 +0000

    cnv calling update

--- protocols/cnv_calling.sh
+++ protocols/cnv_calling.sh
@@ -20,11 +20,11 @@ mkdir -p ${cnvOutDir}
 
 awk '$4 == "CYP2D6" {print $0}' "${pgxGenesBed37}" > "cyp2d6.bed"
 
-python ${asterixRoot}/src/main/python/cnvcaller/core.py correction fit \
+python ${asterixRoot}/src/main/python/cnvcaller/core.py fit \
   --bead-pool-manifest "${bpmFile}" \
   --sample-sheet "${samplesheet}" \
   --bed-file "cyp2d6.bed" \
   --corrective-variants "${correctiveVariantsOutputDir}/merged.prune.in" \
-  --correction "${cnvOutDir}" \
-  --cluster-file "${cnvOutDir}" \
-  --input ${arrayStagedIntensities[@]}
+  --out ${cnvOutDir} \
+  --input ${arrayStagedIntensities[@]} \
+  --config ${asterixRoot}/src/main/python/cnvcaller/conf/config.yml
