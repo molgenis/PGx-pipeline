c09c736f6996770004841f7a89a70d5034e6defb
Robert Warmerdam <robert_warmerdam@hotmail.com>
2022-04-11 15:12:35 +0200

    Filtering samples that pass QC

--- protocols/cnv_calling.sh
+++ protocols/cnv_calling.sh
@@ -7,21 +7,35 @@
 #string cnvBedFile
 #string pipelineRoot
 #string arrayStagedIntensities
-#string samplesheet
+#string sampleList
 
 set -e
 set -u
 
+# First load the RPlus version and set the R package library
+# to obtain a list of samples on which we can perform cnv calling
+module load ${RPlusVersion}
+module list
+
+R_LIBS=${rLibsPath}
+
+mkdir -p ${cnvOutDir}
+
+Rscript ${pipelineRoot}/scripts/map_genotype_samples.R \
+--mapping ${toUgliIdentifiers} \
+--samplesheet ${samplesheet} \
+--gid-col "UGLI_ID" --sid-col "genotyping_name" --out "samples_to_select"
+
+# Now laod the python version and activate the python environment
+# to perform cnv calling
 module load "${pythonVersion}"
 module list
 
 source ${pythonEnvironment}/bin/activate
 
-mkdir -p ${cnvOutDir}
-
 python ${asterixRoot}/src/main/python/cnvcaller/core.py fit \
   --bead-pool-manifest "${bpmFile}" \
-  --sample-sheet "${samplesheet}" \
+  --sample-list "samples_to_select.samples.txt" \
   --variants-prefix "${correctiveVariantsOutputDir}" \
   --out "${cnvOutDir}" \
   --input "${arrayStagedIntensities[@]}" \
