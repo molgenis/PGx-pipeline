5c561b757ec5fa17db0e0e145ba9bc4f673f6965
Robert Warmerdam <robert_warmerdam@hotmail.com>
2024-09-04 16:38:11 +0200

    Added small sample QC step that deals with heterozygosity and missingness on a sample level

--- protocols/sample_filtering.sh
+++ protocols/sample_filtering.sh
@@ -5,17 +5,16 @@ ml ${plink2Version}
 ml ${RPlusVersion}
 export R_LIBS_USER=${rLibsPath}
 
-mkdir -p $(dirname ${genotypesPlinkPrefix})
 mkdir -p $(dirname ${sampleListPrefix})
 
-plink2 --bfile ${genotypesOxfordPrefix}_to_qc --out 'plink2_sample_qc' --missing sample-only --het
+plink2 --bfile ${concatenatedGenotypesOutputDir}/plink_dataset_to_qc --out 'plink2_sample_qc' --missing sample-only --het
 
 Rscript ${pipelineRoot}/scripts/qc_autosomes.R \
 --sample-missingness plink2_sample_qc.smiss \
 --heterozygosity plink2_sample_qc.het \
 --out-prefix qc_out
 
-plink2 --bfile ${genotypesOxfordPrefix}_to_qc --keep qc_out.samples_passed_qc.txt \
---make-bed --out ${genotypesPlinkPrefix}
+plink2 --bfile ${concatenatedGenotypesOutputDir}/plink_dataset_to_qc --keep qc_out.samples_passed_qc.txt \
+--make-bed --out ${concatenatedGenotypesOutputDir}/chr_all
 
-awk 'BEGIN{FS="\t"; OFS=FS}{print $2}' ${genotypesPlinkPrefix}.fam > ${sampleListPrefix}.samples.txt
+awk 'BEGIN{FS="\t"; OFS=FS}{print $2}' ${concatenatedGenotypesOutputDir}/chr_all > ${sampleListPrefix}.samples.txt
--- protocols/variant_filtering.sh
+++ protocols/variant_filtering.sh
@@ -10,14 +10,4 @@ mkdir -p $(dirname ${sampleListPrefix})
 
 plink2 --data ${genotypesOxfordPrefix} 'ref-first' \
 --extract ${variantsPassedQualityControl} \
---make-bed --out 'qc_input_plink_dataset' --missing sample-only --het
-
-Rscript ${pipelineRoot}/scripts/qc_autosomes.R \
---sample-missingness qc_input_plink_dataset.smiss \
---heterozygosity qc_input_plink_dataset.het \
---out-prefix qc_out
-
-plink2 --bfile qc_input_plink_dataset --keep qc_out.samples_passed_qc.txt \
 --make-bed --out ${genotypesPlinkPrefix}
-
-awk 'BEGIN{FS="\t"; OFS=FS}{print $2}' ${genotypesPlinkPrefix}.fam > ${sampleListPrefix}.samples.txt
