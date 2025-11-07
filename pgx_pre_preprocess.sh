6a0d1076bdec9e158f9d8cf7fd830cf78d9d164b
RoanKanninga <roankanninga@gmail.com>
2025-11-07 14:42:18 +0100

    added creating gtc2vcf

--- pgx_pre_preprocess.sh
+++ pgx_pre_preprocess.sh
@@ -63,7 +63,22 @@ if [[ -n "${_sampleSheetColumnOffsets['SentrixPosition_A']+isset}" ]]; then
   SentrixPositionAFieldIndex=$((${_sampleSheetColumnOffsets['SentrixPosition_A']} + 1))
 fi
 
+if [[ -n "${_sampleSheetColumnOffsets['Sample_ID']+isset}" ]]; then
+  sampleIDFieldIndex=$((${_sampleSheetColumnOffsets['Sample_ID']} + 1))
+fi
+
+
+module load gtc2vcf
 count=0
+
+projectNameGDIO="${projectName}_plusGDIO"
+samplesheet="${samplesheetFolder}/${projectNameGDIO}.csv"
+
+mkdir -p "${rawdata}/${projectNameGDIO}/results/vcf/"
+rm -f *'_samples.txt'
+
+mkdir -p "${rawdata}/${projectNameGDIO}/results/vcf/"
+
 while read line
 do
 	if [[ "${count}" == 0 ]]
@@ -73,11 +88,49 @@ do
 
 		gtcFile=$(echo "${line}" | awk -v sb="${sentrixBarcodeAFieldIndex}" -v sp="${SentrixPositionAFieldIndex}" 'BEGIN {FS=","}{print $sb"_"$sp".gtc"}')
 		glaasje=$(echo "${line}" | awk -v sb="${sentrixBarcodeAFieldIndex}" 'BEGIN {FS=","}{print $sb}')
+		sampleID=$(echo "${line}" | awk -v sid="${sampleIDFieldIndex}" 'BEGIN {FS=","}{print $sid}')
 		mkdir -p "${rawdata}/${projectName}/${glaasje}"
 		rsync -v "/groups/umcg-pgx/tmp07/rawdata/gtc/${glaasje}/${gtcFile}" "${rawdata}/${projectName}/${glaasje}/"
+		echo "${glaasje}" >> 'glaasjes.txt'
+		echo -e "${gtcFile%.gtc} ${sampleID}" >> "${glaasje}_samples.txt"
+
 	fi
-		
 done<"${samplesheetFolder}/${projectName}.csv"
+echo "first making vcf files"
+
+sort -V 'glaasjes.txt' | uniq > 'uniqglaasjes.txt'
+out_prefix="${rawdata}/${projectNameGDIO}/results/vcf/"
+ref="/apps/data/1000G/phase1/human_g1k_v37_phiX.fasta"
+bpm_manifest_file="/apps/data/GSAarray/GSAMD-24v3-0-EA_20034606_A1.bpm"
+
+# gtc to vcf	
+while read glaasje
+do
+	echo "processing ${glaasje}.."
+
+	path_to_gtc_folder="${rawdata}/${projectName}/${glaasje}/"
+	
+	bcftools +gtc2vcf -g "${path_to_gtc_folder}" -b "${bpm_manifest_file}" -f "${ref}"  |\
+	bcftools reheader --samples "${glaasje}_samples.txt" |\
+	bcftools sort -Ou -T ./bcftools. | \
+	bcftools norm --no-version -oB "${out_prefix}/${glaasje}_first_output.bcf" -c x -f "${ref}"
+
+	echo "${glaasje} done"
+
+done<'uniqglaasjes.txt'
+
+# split vcf per sample
+while read glaasje
+do
+	bcftools +split "${out_prefix}/${glaasje}_first_output.bcf" -o "${out_prefix}"
+done<'uniqglaasjes.txt'
+
+# bgzip + index
+for i in $(ls "${out_prefix}/"*'.vcf')
+do
+	bgzip "${i}"
+	tabix -p "vcf ${i}.gz"
+done
 
 projectNameGDIO="${projectName}_plusGDIO"
 samplesheet="${samplesheetFolder}/${projectNameGDIO}.csv"
